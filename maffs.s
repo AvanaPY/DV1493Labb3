a6b76dbe1d63de2bc4a08049c8c0c965ccba33ae
iTi1337 <tobiash00@gmail.com>
2021-05-05 16:41:39 +0200

    GetInt Works

--- maffs.s
+++ maffs.s
@@ -23,13 +23,15 @@ outbufOffset:	.quad 0
 	.global main
 main:
 	# call inImage		# Get input from user
-	call getChar
-	movb %al, outbuf
-	leaq outbuf, %rdi
-	call puts		#
+	# call getChar
+	# movb %al, outbuf
+	# leaq outbuf, %rdi
+	# call puts		#
 
-	ret	
+	call inImage
+	call getInt
 
+	ret
 inImage:
 	movq $0, inbufOffset	# Reset inbufOffset to 0
 	leaq inbuf, %rdi	# Point to inbuf
@@ -39,16 +41,68 @@ inImage:
 	ret
 
 getInt:
+	movq $0, %rax 			# resets the int register to 0
+	movb $0, %r8b 			# r(8b = 0) == positive, (r8b = 1) == negative
+	movq inbuf, %rcx		# Move inbufOffset to rcx
+	cmp $0, %rcx 			# if inbuf is 0 or "empty"
+	je inImage
+	movq inbufOffset, %rcx	# Move inbufOffset to rcx
+	cmp $64, %rcx 			# if inbufoffset is above maxed
+	jge inImage
+_spaceCheck:
+	movq inbufOffset, %rcx 	# loads inbufOffset into rcx
+	leaq inbuf, %rdx 		# loads inbuf into rdx
+	movb (%rdx, %rcx, 1), %bl # loads the next character from inbuf
+	cmp $32, %bl 			# checks if the character is a space
+	je _incOneAndLoop 		# loops and checks again
+	jmp _posOrMinCheck
+_incOneAndLoop: 			# loops and checks again
+	inc %rcx
+	movq %rcx, inbufOffset
+	jmp _spaceCheck
+_posOrMinCheck: 			# checks if the first character are either a + or -
+	cmp $45, %bl 			# is minus?
+	je _setMin
+	cmp $43, %bl 			# is plus?
+	je _setPos
+	jmp _loop
+_setMin:
+	mov $1, %r8b
+_setPos:
+	inc %rcx
+	movq %rcx, inbufOffset
+	jmp _loop
+_loop:
+	movq inbufOffset, %rcx 	# loads inbufOffset into rcx
+	leaq inbuf, %rdx 		# loads inbuf into rdx
+	movb (%rdx, %rcx, 1), %bl # loads the next character from inbuf
+	sub $'0', %bl 			# makes the "char" 0 based
+	cmp $0, %bl
+	jl _getIntDone 			# jump if lower than 0 (and therefor not a number)
+	cmp $9, %bl
+	jg _getIntDone 			# jump if greater than 9(and therefor not a number)
+_continueLoop: 				# continues the loop by multiplying the sum by 10 and adding the new number
+	imul $10, %rax 
+	add %bl, %al
+_moveForwardLoop: 			# increases inbufOffset with one and loops again
+	inc %rcx
+	movq %rcx, inbufOffset
+	jmp _loop
+_makeNegative: 				# makes the sum negative before returning
+	neg %rax
 	ret
+_getIntDone:
+	cmp $1, %r8b 			# if r8b == 1 (aka the number had a - before it)
+	je _makeNegative 		# makes the sum negative before returning
+	ret 					# answer should sit in rax
+
 getText:
 	ret
 getChar:
 	leaq inbuf, %rax 	# move the adress of inbuf to rax
 	movq inbufOffset, %rbx	# Move inbufOffset to rbx
-
 	cmp $0, %rbx 		# if inbufOffset is 0
 	je _getCharNotDone
-
 	cmp $64, %rbx 		# if inbufoffset is above maxed
 	je _getCharNotDone
 _getCharContinue:
@@ -59,7 +113,6 @@ _getCharNotDone:
 	call inImage		# grab new input
 	movq inbufOffset, %rbx	# Move inbufOffset to rbx
 	jmp _getCharContinue
-
 getInPos:
 	mov inbufOffset, %rax
 	ret
