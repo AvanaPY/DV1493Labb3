1902a58308aa6850105b2d19b1e4a0141c007f52
Emil Karlström <emil.karlstrom@gmail.com>
2021-05-06 13:41:21 +0200

    setOutPos works

--- maffs.s
+++ maffs.s
@@ -30,8 +30,16 @@ main:
 #	call getInt
 #	call getInt	
 #	movq $0, %rbx
-	movq $78, %rdi	
-	call setInPos
+	movq $8, %rdi	
+	call setOutPos
+	
+	movq $-5, %rdi	
+	call setOutPos
+	
+	movq $70, %rdi	
+	call setOutPos
+	
+	call getOutPos
 	ret
 
 #############
@@ -52,11 +60,6 @@ inImage:
 # Reads from the current inbuf buffer position
 # and converts the text into a number that it returns in %rax
 #
-# Arguments:
-#
-# Returns:
-#	%rax - A single int
-#
 ##############
 getInt:
 	movq $0, %rax 			# resets the int register to 0
@@ -67,30 +70,30 @@ getInt:
 	movq inbufOffset, %rcx	# Move inbufOffset to rcx
 	cmp $64, %rcx 			# if inbufoffset is above maxed
 	jge inImage
-_getIntSpaceCheck:
+_spaceCheck:
 	movq inbufOffset, %rcx 	# loads inbufOffset into rcx
 	leaq inbuf, %rdx 		# loads inbuf into rdx
 	movb (%rdx, %rcx, 1), %bl # loads the next character from inbuf
 	cmp $32, %bl 			# checks if the character is a space
-	je _getIntincOneAndLoop 		# loops and checks again
-	jmp _getIntposOrMinCheck
-_getIntincOneAndLoop: 			# loops and checks again
+	je _incOneAndLoop 		# loops and checks again
+	jmp _posOrMinCheck
+_incOneAndLoop: 			# loops and checks again
 	inc %rcx
 	movq %rcx, inbufOffset
-	jmp _getIntSpaceCheck
-_getIntposOrMinCheck: 			# checks if the first character are either a + or -
+	jmp _spaceCheck
+_posOrMinCheck: 			# checks if the first character are either a + or -
 	cmp $45, %bl 			# is minus?
-	je _getIntsetMin
+	je _setMin
 	cmp $43, %bl 			# is plus?
-	je _getIntsetPos
-	jmp _getIntloop
-_getIntsetMin:
+	je _setPos
+	jmp _loop
+_setMin:
 	mov $1, %r8b
-_getIntsetPos:
+_setPos:
 	inc %rcx
 	movq %rcx, inbufOffset
-	jmp _getIntloop
-_getIntloop:
+	jmp _loop
+_loop:
 	movq inbufOffset, %rcx 	# loads inbufOffset into rcx
 	leaq inbuf, %rdx 		# loads inbuf into rdx
 	movb (%rdx, %rcx, 1), %bl # loads the next character from inbuf
@@ -99,17 +102,19 @@ _getIntloop:
 	jl _getIntDone 			# jump if lower than 0 (and therefor not a number)
 	cmp $9, %bl
 	jg _getIntDone 			# jump if greater than 9(and therefor not a number)
+_continueLoop: 				# continues the loop by multiplying the sum by 10 and adding the new number
 	imul $10, %rax 
 	add %bl, %al
+_moveForwardLoop: 			# increases inbufOffset with one and loops again
 	inc %rcx
 	movq %rcx, inbufOffset
-	jmp _getIntloop
-_getIntmakeNegative: 				# makes the sum negative before returning
+	jmp _loop
+_makeNegative: 				# makes the sum negative before returning
 	neg %rax
 	ret
 _getIntDone:
 	cmp $1, %r8b 			# if r8b == 1 (aka the number had a - before it)
-	je _getIntmakeNegative 		# makes the sum negative before returning
+	je _makeNegative 		# makes the sum negative before returning
 	ret 					# answer should sit in rax
 
 ##############
@@ -252,6 +257,7 @@ _putCharDone:
 #
 ####################
 getOutPos:
+	mov outbufOffset, %rax
 	ret
 
 ##################
@@ -264,5 +270,17 @@ getOutPos:
 ##################
 
 setOutPos:
+	cmp $0, %rdi
+	jl _setOutPosLZ
+	cmp maxBufferSize, %rdi
+	jg _setOutPosGM
+	jmp _setOutPosDone 	
+_setOutPosLZ:
+	movq $0, %rdi
+	jmp _setOutPosDone
+_setOutPosGM:
+	movq maxBufferSize, %rdi
+_setOutPosDone:	
+	mov %rdi, outbufOffset		#
 	ret
 
